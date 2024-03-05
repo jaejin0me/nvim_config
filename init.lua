6c45d148b37daf01dbba041aeb7d9a3e04756b74
Jaejin Jang <jaejin0me@gmail.com>
2024-03-05 23:42:07 +0900

    syntax tree 패키지 treesitter 추가

--- init.lua
+++ init.lua
@@ -24,7 +24,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
@@ -35,6 +36,13 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = { "lua", "javascript" },
+    highlight = { enable = true },
+    indent = { enable = true }
+})
 
 -- colorschme 설정
 require("catppuccin").setup()
