8cb87d2d08279bd3df651c3289f95fe7e735601a
Jaejin Jang <jaejin0me@gmail.com>
2024-03-10 11:59:20 +0900

    Add neo-tree and modularize config

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
--- 기본설정
-vim.o.number = true
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 -- 패키지 매니저 lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -20,31 +12,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
 -- lazy.nvim 호출
-require("lazy").setup(plugins, opts)
-
--- telescope 설정
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = { "lua", "javascript" },
-    highlight = { enable = true },
-    indent = { enable = true }
-})
-
--- colorschme 설정
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+require("vim-options")
+require("lazy").setup('plugins')
