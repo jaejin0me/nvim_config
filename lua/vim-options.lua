cd97b4c83037f1a66a95607afb095660f501ffd2
Jaejin Jang <jaejin0me@gmail.com>
2024-03-29 16:36:24 +0900

    neo-tree 파일시스템 토글로 변경, 윈도우간 이동 단축키 설정 추가

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+
+-- Move window
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
