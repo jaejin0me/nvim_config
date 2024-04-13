f22ed71d87db680f23d648c93ca5c78d28bda516
Jaejin Jang <jaejin0me@gmail.com>
2024-04-13 11:18:05 +0900

    alpha 플러그인 삭제 일부 단축키 직관적으로 변경

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -22,6 +22,6 @@ return {
 		end
 
 		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
 	end,
 }
